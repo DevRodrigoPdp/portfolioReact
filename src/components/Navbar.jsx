e83b29670fb07378a66e7472dcc4ca715e2be024
Rodrigo Puerta <rpuertadelpozo@gmail.com>
2025-04-10 08:52:11 +0200

    Update NavBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,31 +18,28 @@ export const Navbar = () => {
         scrolled
           ? "bg-gray-800 bg-opacity-80 backdrop-blur-md"
           : "bg-transparent"
-      } mx-auto`}
+      } w-full max-w-screen-lg mx-auto`}
     >
-      <div className="flex items-center gap-6 ">
-        <a
-          href="#about"
-          className="title-font font-medium text-white mb-4 md:mb-0 ml-3 text-xl"
-        >
+      <div className="flex items-center justify-center gap-6 w-full max-w-screen-md mx-auto">
+        <a href="#about" className="title-font font-medium text-white text-xl">
           Rodrigo Puerta
         </a>
 
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center">
-          <a href="#experiencia" className="mr-5 hover:text-white">
+        <nav className="flex flex-wrap items-center text-base justify-center gap-5">
+          <a href="#experiencia" className="hover:text-white">
             Experiencia
           </a>
-          <a href="#projects" className="mr-5 hover:text-white">
+          <a href="#projects" className="hover:text-white">
             Trabajos anteriores
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+          <a href="#skills" className="hover:text-white">
             Habilidades
           </a>
           <a
             href="https://mail.google.com/mail/?view=cm&fs=1&to=rpuertadelpozo@gmail.com&su=Interesado%20en%20tu%20trabajo&body=Hola%20Rodrigo%2C%20me%20gustaría%20contactarte..."
             target="_blank"
             rel="noopener noreferrer"
-            className="mr-5 hover:text-white"
+            className="hover:text-white"
           >
             Contacto
           </a>
